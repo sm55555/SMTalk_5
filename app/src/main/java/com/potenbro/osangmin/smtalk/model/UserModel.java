d5eb9d30f70f0e69771d08240cb5ad459d33182f
오상민 <osangmin@osangmin-ui-MacBook-Pro.local>
2018-08-03 21:36:38 +0900

    Initial commit

---
+++ app/src/main/java/com/potenbro/osangmin/smtalk/model/UserModel.java
@@ -0,0 +1,10 @@
+package com.potenbro.osangmin.smtalk.model;
+
+/**
+ * Created by osangmin on 2018. 8. 3..
+ */
+
+public class UserModel {
+    public String userName;
+    public String profieImageUrl;
+}
